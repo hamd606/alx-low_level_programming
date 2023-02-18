f86ba2c53fea1423095d08638683ad3b1f6c8817
Your Name <you@example.com>
2023-02-18 04:45:52 -0800

    gg

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - code entry poinit
+ *
+ * Return: 0 hopefully
+ */
+
+int main(void)
+{
+	char charSize;
+	int intSize;
+	long int longintSize;
+	long long int llongSiez;
+	float floatSize;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charSize));
+	printf("Size of a int: %zu byte(s)\n", sizeof(intSize));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintSize));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(llongSiez));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatSize));
+
+	return (0);
+
+}
