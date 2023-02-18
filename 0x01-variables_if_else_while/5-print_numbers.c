105bd040f2703d8d84130dd554e8ce1a64608bfb
Your Name <you@example.com>
2023-02-18 03:30:51 -0800

    fr

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Check if a number is greater than 0
+*
+* Return: 1 if the number is positive. 0 otherwise
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 10 ; n++)
+	{
+		printf("%d", n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
