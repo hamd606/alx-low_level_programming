cee5cfbbe2bfb7901cc2e488fc3582b1473040b4
Your Name <you@example.com>
2023-02-18 02:43:54 -0800

    fr

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Check if a number is greater than 0
+*
+* Return: 1 if the number is positive. 0 otherwise
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
