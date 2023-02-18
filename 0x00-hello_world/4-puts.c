791036362e5fdadfbf1cc677939c0a8b698688e9
Your Name <you@example.com>
2023-02-18 04:56:54 -0800

    fg

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - code entry poinit
+ *
+ * Return: 0 hopefully
+ */
+
+int main(void)
+{
+	char phrase[52];
+
+	strcpy(phrase, "\"Programming is like building a multilingual puzzle");
+
+	puts(phrase);
+
+	return (0);
+
+}
