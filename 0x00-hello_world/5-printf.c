39819b8c44c7e9379ebb6f9c2b5e6bf781843c24
Your Name <you@example.com>
2023-02-18 04:30:14 -0800

    44

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - code entry poinit
+ *
+ * Return: 0 hopefully
+ */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, ");
-	return 0;
-}
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
+}
