32f20a7bdaefe5618526f523041a12d6c3202a10
Your Name <you@example.com>
2023-02-20 05:18:06 -0800

    ft

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - code entry poinit
+ *
+ * Return: 1 as the idea is to print to sterr
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful");
+	fprintf(stderr, "- Dora Korpar, 2015-10-19\n");
+
+	return (1);
+
+}
