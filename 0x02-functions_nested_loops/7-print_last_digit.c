8b8313398e2050c67bd3532e221f6fdf4be459a1
Your Name <you@example.com>
2023-02-21 12:07:05 -0800

    auto commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_last_digit - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: On false 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_last_digit(int);
+{
+	return (n % 10);
+}
