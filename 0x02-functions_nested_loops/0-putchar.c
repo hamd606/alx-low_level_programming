f70758b0c43a47aaa39669888262cc24b1626461
Your Name <you@example.com>
2023-02-21 06:28:38 -0800

    frr

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - code entrypoint
+ *
+ * Return: 0 usually
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is a test repo nothing meaningful here
