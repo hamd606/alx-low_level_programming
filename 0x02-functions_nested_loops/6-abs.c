bfde7677d92e640e74410577579c6819f185b581
Your Name <you@example.com>
2023-02-21 11:47:06 -0800

    auto commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: On false 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+
+	else
+		return (n * ((-1));
+}
