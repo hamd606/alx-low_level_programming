7e964724d7e740f02b8c108fe3e67ed88266af7e
Your Name <you@example.com>
2023-02-22 06:42:58 -0800

    auto commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * add - adds two numbers
  * @n0: first term
- * @n2: second term
+ * @n1: second term
  *
  * Return: the result.
  * On error, -1 is returned, and errno is set appropriately.
