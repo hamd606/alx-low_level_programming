e55adb83cbaba3ac4da99270845dafec4c8e133b
Your Name <you@example.com>
2023-02-22 06:28:09 -0800

    auto commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * times table - writes the character c to stdout
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void times_table(void)
+{
+	int i, j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i * j < 10)
+			{
+				_putchar((i * j) +'0');
+				
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+
+					if (i * ( j + 1) < 10)
+						_putchar(' ');
+				}
+			}
+
+			else
+			{
+				_putchar((i * j / 10) + '0');
+				_putchar((i * j % 10) + '0');
+
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+
+		_putchar('\n');
+	}
+
+	return (0);
+}
