c476426a608e5b51b3b785e1677f7c383cfc2dde
Your Name <you@example.com>
2023-02-22 06:33:37 -0800

    auto commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,20 +8,21 @@
 void times_table(void)
 {
 	int i, j;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			if (i * j < 10)
 			{
-				_putchar((i * j) +'0');
-				
+				_putchar((i * j) + '0');
+
 				if (j < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 
-					if (i * ( j + 1) < 10)
+					if (i * (j + 1) < 10)
 						_putchar(' ');
 				}
 			}
