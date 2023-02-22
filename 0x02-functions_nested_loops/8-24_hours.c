0d44fa89e32b074fd8da694e14e1f7f63558985d
Your Name <you@example.com>
2023-02-22 05:56:45 -0800

    auto commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * print_last_digit - writes the character c to stdout
- * @n: The character to print
+ * jack_bauer - writes the character c to stdout
  *
- * Return: On false 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
@@ -17,11 +15,11 @@ void jack_bauer(void)
 	{
 		for (j = 0; j <= 59; j++)
 		{
-			_putchar((i/10)+'0');
-			_putchar((i%10)+'0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((j/10)+'0');
-			_putchar((j%10)+'0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 		}
 	}
