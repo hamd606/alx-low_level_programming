d27972ba712fbb0a9d071a75ca2304617d2ea18a
Your Name <you@example.com>
2023-02-22 05:14:36 -0800

    auto commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,6 +11,15 @@
 
 int print_last_digit(int n)
 {
-	_putchar(n % 10);
-	return (n % 10);
+	if (n >= 0)
+	{
+		/*_putchar('0' + (n % 10));*/
+		return (0 + (n % 10));
+	}
+
+	else
+	{
+		/*_putchar('0' + (-1 * n % 10));*/
+		return (0 + (-1 * n % 10));
+	}
 }
