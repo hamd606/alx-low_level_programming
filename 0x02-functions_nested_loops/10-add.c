a5ce4de776ccc2525bd82ed3df662b96bd382b54
Your Name <you@example.com>
2023-02-22 06:39:36 -0800

    auto commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+
+/**
+ * add - adds two numbers
+ * @n0: first term
+ * @n2: second term
+ *
+ * Return: the result.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int n0, n1)
+{
+	return (n0 + n0);
+}
