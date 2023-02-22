db2a1b481a1113345f6db4a371c75d39422e065e
Your Name <you@example.com>
2023-02-22 08:14:23 -0800

    auto commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two numbers
+ * @n0: first term
+ * @n1: second term
+ *
+ * Return: the result.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n == 98)
+	{
+		printf("98");
+	}
+	
+	else if (n < 98)
+	{
+		for(i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		
+		printf("98");
+		
+	}
+	else if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+
+		printf("98");
+
+	}
+	
+	_putchar('\n');
+
+
+
+}
