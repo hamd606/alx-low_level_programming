8a735123f502d2e2474330326e5cedeb34624c35
Your Name <you@example.com>
2023-02-23 04:05:17 -0800

    auto commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,15 @@
 
 void print_number(int n)
 {
-	unsigned int i;
+	unsigned int x;
 
-	i = n;
+	x = n;
 	if (n < 0)
 	{
 		_putchar(45);
-		i = -n;
+		x = -n;
 	}
-	if (i / 10)
-		print_number(i / 10);
-	_putchar((i % 10) + '0');
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
