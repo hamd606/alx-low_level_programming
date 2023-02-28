f4b71a044d79ecdd991797b0f7d1c4b8808b6c2a
Your Name <you@example.com>
2023-02-28 10:51:18 -0800

    auto commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 	int str_lenght, i, half;
 	char tmp;
 
-	for (str_lenght = 0; s[str_length] != '\0'; str_length++)
+	for (str_length = 0; s[str_length] != '\0'; str_length++)
 		;
 	i = 0;
 	half = str_length / 2;
