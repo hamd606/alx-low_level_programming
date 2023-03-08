c94697428f1f3418d3819b5bed72efbb60be8605
Your Name <you@example.com>
2023-03-08 11:41:36 -0800

    auto commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev__recursion - prints a reversed str using _putchar
+ * _print_rev_recursion - prints a reversed str using _putchar
  *@s: input str
  */
 void _print_rev_recursion(char *s)
