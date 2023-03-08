b7648db878e42ec4a700c1d1bc19efc7b4cc4112
Your Name <you@example.com>
2023-03-08 12:40:55 -0800

    auto commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+
+#include "main.h"
+
+/**
+ * is_prime_number - checks if prime
+ * @n: input
+ * Return: 1 if true 0 otherwise
+ */
+int is_prime_number(int n);
+{
+	int i;
+
+	i = 2;
+
+	if(n == i)
+		return 0;
+	else
+		if(n % i == 0)
+			return (1);
+		else
+		{
+			return (is_prime_number(i + 1, n));
+		}
+}
