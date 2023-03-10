b47d0e8aae819038962fd704d024a9b5e8f71ef7
Your Name <you@example.com>
2023-03-10 15:01:51 -0800

    auto commit

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - the same as puts
+ * @str: pointer input
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
