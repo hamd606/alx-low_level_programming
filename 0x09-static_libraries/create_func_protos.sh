eb7c39f2932dea115c774801bdc650dedc0e8c75
Your Name <you@example.com>
2023-03-10 10:30:37 -0800

    auto commit

---
+++ 0x09-static_libraries/create_func_protos.sh
@@ -0,0 +1,21 @@
+echo 'int _putchar(char c);' > 0-isupper.c
+echo 'int _islower(int c);' > 0-memset.c
+echo 'int _isalpha(int c);' > 0-strcat.c
+echo 'int _abs(int n);' > 1-isdigit.c
+echo 'int _isupper(int c);' > 1-memcpy.c
+echo 'int _isdigit(int c);' > 1-strncat.c
+echo 'int _strlen(char *s);' > 100-atoi.c
+echo 'void _puts(char *s);' > 2-strchr.c
+echo 'char *_strcpy(char *dest, char *src);' > 2-strlen.c
+echo 'int _atoi(char *s);' > 2-strncpy.c
+echo 'char *_strcat(char *dest, char *src);' > 3-islower.c
+echo 'char *_strncat(char *dest, char *src, int n);' > 3-puts.c
+echo 'char *_strncpy(char *dest, char *src, int n);' > 3-strcmp.c
+echo 'int _strcmp(char *s1, char *s2);' > 3-strspn.c
+echo 'char *_memset(char *s, char b, unsigned int n);' > 4-isalpha.c
+echo 'char *_memcpy(char *dest, char *src, unsigned int n);' > 4-strpbrk.c
+echo 'char *_strchr(char *s, char c);' > 5-strstr.c
+echo 'unsigned int _strspn(char *s, char *accept);' > 6-abs.c
+echo 'char *_strpbrk(char *s, char *accept);' > 9-strcpy.c
+echo 'char *_strstr(char *haystack, char *needle);' > _putchar.c
+
