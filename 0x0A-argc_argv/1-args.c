c802d2d7a3a2f964a0809a691f612959957dd4be
Your Name <you@example.com>
2023-03-11 01:36:50 -0800

    hope

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints argc passed from cmd
- * @argc: argc main 
+ * @argc: argc main
  * @argv: args passed to the prog
  * Return: 0
  */
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc > 0)
 		printf("%d\n", argc - 1);
-	
+
 	else
 	{
 		printf("%s", argv[0]);
