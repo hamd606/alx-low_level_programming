db207f661864e8890534212c1181176c1308dbb9
Your Name <you@example.com>
2023-03-11 01:57:32 -0800

    hope

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints argc passed from cmd
+ * @argc: argc main
+ * @argv: args passed to the prog
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
