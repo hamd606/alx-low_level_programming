0e2a4f5d4fbeb17237e3c6617b1271272e39621f
Your Name <you@example.com>
2023-03-13 13:48:54 -0700

    Keeping it going

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.>
+
+
+/**
+  * create_array - create a dinamic array of size size
+  * @size: size of the array
+  * @c: the char to initialize the block with
+  * Return: pointer to the reserved memory bloc 
+  */
+char *create_array(unsigned int size, char c)
+{
+
+	int i;
+	
+	char *p;
+
+	if (size == 0)
+		return (NULL);
+
+	p = malloc(size);
+	
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
+	return (p);
+
+}
---
+++ 0x0B-malloc_free/push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git add .
+git commit -m 'Keeping it going'
+git push
