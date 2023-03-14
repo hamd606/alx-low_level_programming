125584aff4ad3122c1afeb2d27bfbac2e7ea0720
Your Name <you@example.com>
2023-03-14 11:37:34 -0700

    Keeping it going

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,39 +17,44 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	len2 = 0;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		p = malloc(1);
-		p[1] = '\0';
-	}
-
 	while (s1[len1])
 		len1++;
 
 	while (s2[len2])
 		len2++;
 
-	if (len1 + len2 == 0)
+	if (s1 == NULL && s2 == NULL)
 	{
 		p = malloc(1);
-		p[1] = '\0';
+		p[0] = '\0';
+	}
+
+	else if (s1 == NULL || len1 == 0)
+	{
+		p = malloc(len2 + 1);
+		for (i = 0; i < len2; i++)
+			p[i] = s2[i];
+		p[i + 1] = '\0';
+	}
+
+	else if (s2 == NULL || len2 ==0)
+	{
+		p = malloc(len1 + 1);
+		for (i = 0; i < len1; i++)
+			p[i] = s1[i];
+		p[i + 1] = '\0';
 	}
 
 	else
 	{
-		p = malloc(len1 + len2 + 1);
+		p = malloc (len1 + len2 + 1);
+		for (i = 0; i < len1; i++)
+			p[i] = s1[i];
+		for (i = len1; i < (len1 + len2); i++)
+			p[i] = s2[i - len1];
+		p[len1 + len2] = '\0';
+
 	}
-
-	if (p == NULL)
-		return (NULL);
-
-	for (i = 0; i < len1; i++)
-		p[i] = s1[i];
-
-	for(i = 0; i < len2; i++)
-		p[i + len1] = s2[i];
-
-	p[i + 1] = '\0';
 
 	return (p);
 
