5686571131005b07762ceebd82265aab5fd5639f
Your Name <you@example.com>
2023-03-14 13:06:30 -0700

    Keeping it going

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,37 +17,55 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	len2 = 0;
 
-	while (s1[len1])
-		len1++;
-
-	while (s2[len2])
-		len2++;
+	if (s1 != NULL)
+	{
+		while (s1[len1])
+			len1++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[len2])
+			len2++;
+	}
 
 	if (s1 == NULL && s2 == NULL)
 	{
 		p = malloc(1);
+		if (p == NULL)
+			return (NULL);
+		
 		p[0] = '\0';
 	}
 
 	else if (s1 == NULL || len1 == 0)
 	{
 		p = malloc(len2 + 1);
+		if (p == NULL)
+			return (Null);
+
 		for (i = 0; i < len2; i++)
 			p[i] = s2[i];
+
 		p[i + 1] = '\0';
 	}
 
 	else if (s2 == NULL || len2 ==0)
 	{
 		p = malloc(len1 + 1);
+		if (p == NULL)
+			return (NULL);
+
 		for (i = 0; i < len1; i++)
 			p[i] = s1[i];
+
 		p[i + 1] = '\0';
 	}
 
 	else
 	{
 		p = malloc (len1 + len2 + 1);
+		if (p == NULL)
+			return (NULL);
 		for (i = 0; i < len1; i++)
 			p[i] = s1[i];
 		for (i = len1; i < (len1 + len2); i++)
