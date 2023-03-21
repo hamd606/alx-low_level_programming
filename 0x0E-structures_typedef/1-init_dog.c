0dbcabed9e5776df446f358fa812bc43ad752cd7
Your Name <you@example.com>
2023-03-21 12:00:19 -0700

    msg

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdio.h>
 /**
   * init_dog - initializes dog struct
   * @d: struct pointer
@@ -12,4 +12,13 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	(*d).owner = owner;
+}
+
+int main(void)
+{
+    struct dog my_dog;
+
+    init_dog(&my_dog, "Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    return (0);
 }
