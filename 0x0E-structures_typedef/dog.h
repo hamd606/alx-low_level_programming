d92058a1187503f1ca56d8a07691f1b6223c037f
Your Name <you@example.com>
2023-03-21 11:42:09 -0700

    msg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
+/**
+ * struct dog - a dog :)
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
