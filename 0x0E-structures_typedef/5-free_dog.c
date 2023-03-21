4830bfd4943e5547ae2e2e7e2b78a8149bd3fa4a
Your Name <you@example.com>
2023-03-21 13:00:12 -0700

    msg

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dog from memory
+ * @d: pointer to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d);
+}
