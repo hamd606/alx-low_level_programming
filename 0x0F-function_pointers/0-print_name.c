af30317bcca2d17d473a8c082e0328b7d811d108
Your Name <you@example.com>
2023-03-22 12:13:09 -0700

    audo msg

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 /**
  * print_name - prints a name
  * @name: name to be printed
@@ -5,5 +6,11 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+		return;
+
+	if (f == NULL)
+		return;
+
 	f(name);
 }
