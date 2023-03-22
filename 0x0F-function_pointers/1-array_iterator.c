e91e00ee38decaf115e3cf690f0a5db988012e9d
Your Name <you@example.com>
2023-03-22 12:20:02 -0700

    audo msg

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+
+/**
+ * array_iterator - executes a function parameter
+ * @array: arr
+ * @size: size of arr
+ * @action: ptr to funtionto be executed
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL)
+		return;
+	if ( action == NULL)
+		return;
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
