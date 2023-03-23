bc8903305ec27c33086ff0b3f46612dc81d7a52a
Your Name <you@example.com>
2023-03-23 13:35:22 -0700

    commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,36 +15,29 @@ void print_all(const char * const format, ...)
 	char *str;
 
 	va_start(valist, format);
-
 	while (format && format[i])
 		i++;
-
 	while (format && format[n])
 	{
 		if (n  == (i - 1))
 		{
 			sep = "";
-		}	
-
+		}
 		switch (format[n])
 		{
 			case 'c':
 			printf("%c%s", va_arg(valist, int), sep);
 			break;
-
 			case 'i':
 			printf("%d%s", va_arg(valist, int), sep);
 			break;
-
 			case 'f':
 			printf("%f%s", va_arg(valist, double), sep);
 			break;
-			
 			case 's':
 			str = va_arg(valist, char *);
 			if (str == NULL)
 				str = "(nil)";
-			
 			printf("%s%s", str, sep);
 			break;
 		}
