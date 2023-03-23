52f7e910ad05486b937089d8c5ff321959df36ce
Your Name <you@example.com>
2023-03-23 10:35:18 -0700

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,16 +8,19 @@
   */
 int sum_them_all(const unsigned int n, ...)
 {
-	int i;
+	unsigned int i;
 	int result = 0;
+	va_list args_list;
+
+	va_start(args_list, n);
 
 	if (n == 0)
 		return (0);
-
+	
 	else
 	{
 		for (i = 0; i < n; i++)
-			result += i;
+			result += va_arg(args_list, int);
 	}
 
 	return (result);
