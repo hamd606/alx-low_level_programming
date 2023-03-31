4912c9766ddaa74b2a537a898a871071b6a87c30
CHIKHAOUI Mohamed <hamd0001@gmail.com>
2023-03-31 11:32:27 -0700

    fixed : styling

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,12 +8,12 @@ void free_list(list_t *head)
 {
 	list_t *current_node;
 	list_t *next_node;
-	
+
 	if (head)
 	{
 		current_node = head;
 		next_node = head->next;
-	
+
 		while (next_node)
 		{
 			free(current_node->str);
@@ -21,7 +21,7 @@ void free_list(list_t *head)
 			current_node = next_node;
 			next_node = next_node->next;
 		}
-		
+
 		free(current_node->str);
 		free(current_node);
 	}
