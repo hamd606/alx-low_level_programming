4697f3315ba42e3f83f5de3e07e02fd9e0bf7563
CHIKHAOUI Mohamed <hamd0001@gmail.com>
2023-04-04 08:55:20 -0700

    Initial try

---
+++ 0x13-more_singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - counts the number of nodes in a linkeed list
+ * @h: ptr to the list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the begining of a list
+ * @head: ptr to list to add a node to
+ * @str: ptr to data to be added
+ * Return: updated list and NULL at failure
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int i;
+	list_t *node;
+
+	node  = malloc(sizeof(list_t *));
+
+	if (str == NULL)
+	{
+		node->str = NULL;
+		node->len = 0;
+		node->next = *head;
+	}
+
+	if (node == NULL)
+		return (NULL);
+
+	node->next = *head;
+	*head = node;
+	node->str = strdup(str);
+
+	for (i = 0; str[i]; i++)
+		;
+
+	node->len = i;
+
+	return (node);
+}
