794be6fdec646676a8f14f228daacf3bd758f955
CHIKHAOUI Mohamed <hamd0001@gmail.com>
2023-04-04 16:06:32 -0700

    fixing all the above, hopefully

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,7 +4,7 @@
  * free_listint2 - frees a listint_t
  * @head: pointer to the list to be free'd
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *current_node;
 
