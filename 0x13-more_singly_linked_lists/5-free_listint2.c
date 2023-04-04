c70e86e8b7318ac66db64c1bbc628d3450b0696e
CHIKHAOUI Mohamed <hamd0001@gmail.com>
2023-04-04 15:55:40 -0700

    task checkout

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t
+ * @head: pointer to the list to be free'd
+ */
+void free_listint2(listint_t *head)
+{
+	listint_t *current_node;
+
+	if (head == NULL)
+		return;
+
+	if (*head == NULL)
+		return;
+
+	while(head)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node);
+	}
+
+	head = NULL;
+}
