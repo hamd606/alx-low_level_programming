9e1bc07aa87740d43381ad88cdd2da32b2bfc2e0
CHIKHAOUI Mohamed <hamd0001@gmail.com>
2023-04-08 13:13:58 -0700

    next task

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 /**
  * clear_bit - set bit 0 at given index
  * @n: ptr input
- * @index: index of bit 
+ * @index: index of bit
  * Return: 1 on succes -1 otherwise
  */
 int clear_bit(unsigned long int *n, unsigned int index)
