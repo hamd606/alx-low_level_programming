35836d6d09704586cb73fa0ce2c285bcc313523d
CHIKHAOUI Mohamed <hamd0001@gmail.com>
2023-04-08 08:29:19 -0700

    adding drclaration

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,6 @@ void rev_string(char *s);
 int power(int base, int power);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
