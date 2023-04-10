5ed415106e8605d0a592ea5f89c7aebff45a057d
CHIKHAOUI Mohamed <hamd0001@gmail.com>
2023-04-10 13:15:51 -0700

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * append_text_to_file - appends text to a file
+  * @filename: file name input
+  * @text_content: text to be appended
+  * Return: 1 oon success -1 otherwise
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, str_len;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	str_len = 0;
+
+	while (text_context[str_len])
+		str_len++;
+
+	if (text_content)
+	{
+		if (write(fd, text_content, _strlen(text_content)) == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
