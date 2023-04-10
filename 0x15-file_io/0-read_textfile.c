73a6f5cee8661933d79709710cad295b0d1185b6
CHIKHAOUI Mohamed <hamd0001@gmail.com>
2023-04-10 09:25:52 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * read_textfile - read a text file and prints it
+  * @filename: text file
+  * @letters: number of chars to reads
+  * Return: number of chars printed
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, len;
+	char *buff; 
+
+	buff = malloc(sizeof(char *) * letters);
+
+	if (buff == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	len = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, len);
+
+	free(buff);
+	close(fd);
+
+	return (len);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_h
+#define MAIN_H
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
