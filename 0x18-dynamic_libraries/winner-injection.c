6cbad7ffee3168870b589a249c462392c4c8950b
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-08-21 04:33:56 -0700

    last task test

---
+++ 0x18-dynamic_libraries/winner-injection.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int printf(const char *format, ...)
+{
+	puts("9 8 10 24 75 9");
+	puts("Congratulations, you win the Jackpot!");
+	exit(EXIT_SUCCESS);
+
+}
